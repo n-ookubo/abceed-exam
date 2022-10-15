a352054a7ff3e7c9a73a34a1fd649b3b2e564aca
Naoaki Okubo <ohkubo.n.ab@gmail.com>
2022-10-16 06:00:30 +0900

    import AbceedUILibrary and change Directory

---
+++ abceed-exam/Sources/AbceedExtension.swift
@@ -0,0 +1,13 @@
+//
+//  AbceedExtension.swift
+//  abceed-exam
+//
+//  Created by Naoaki Okubo on 2022/10/16.
+//
+
+import AbceedUILibrary
+import UIKit
+
+extension Abceed where Base: UIColor {
+    public static var myred: UIColor { .init(named: "MyRed")! }
+}
