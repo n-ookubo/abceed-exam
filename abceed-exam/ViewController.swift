af2dd78a813b6ce8df37b1ccf53baee31a589f38
Naoaki Okubo <ohkubo.n.ab@gmail.com>
2022-10-16 05:41:02 +0900

    Initial Commit

---
+++ abceed-exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  abceed-exam
+//
+//  Created by Naoaki Okubo on 2022/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
