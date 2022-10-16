c2f6a8e14614402abb5bfe6ffb19afb852147623
Naoaki Okubo <ohkubo.n.ab@gmail.com>
2022-10-16 15:59:32 +0900

    implementation button style func

--- abceed-exam/Sources/UI/UILib.swift
+++ abceed-exam/Sources/UI/UILib.swift
@@ -5,6 +5,7 @@
 //  Created by Naoaki Okubo on 2022/10/16.
 //
 
+import AbceedUILibrary
 import AlamofireImage
 import Foundation
 import UIKit
@@ -49,6 +50,70 @@ class UILib {
             indicatorVisible = false
         }
         indicatorLock.unlock()
+    }
+}
+
+extension UI {
+    public enum ButtonStyle : Int {
+        case redBorder = 0
+        case redBody
+        case gray
+        public var foregroundColor: UIColor {
+            switch self {
+            case .redBorder: return Abceed.myred
+            case .redBody: return Abceed.monotone8
+            case .gray: return Abceed.monotone3
+            }
+        }
+        public var backgroundColor: UIColor {
+            switch self {
+            case .redBorder: return Abceed.monotone8
+            case .redBody: return Abceed.myred
+            case .gray: return Abceed.monotone8
+            }
+        }
+        public var borderColor: UIColor {
+            switch self {
+            case .redBorder: return Abceed.myred
+            case .redBody: return Abceed.myred
+            case .gray: return Abceed.monotone3
+            }
+        }
+        public var configBase: UIButton.Configuration {
+            switch self {
+            case .redBorder: return UIButton.Configuration.plain()
+            case .redBody: return UIButton.Configuration.filled()
+            case .gray: return UIButton.Configuration.plain()
+            }
+        }
+    }
+    
+    public static func buttonConfig(
+        style: ButtonStyle,
+        title: String? = nil,
+        fontSize: CGFloat = 12.0,
+        fontWeight: UIFont.Weight = .regular,
+        buttonSize: UIButton.Configuration.Size = .small,
+        borderWidth: CGFloat = 1.0,
+        borderRadius: CGFloat = 5.0
+    ) -> UIButton.Configuration {
+        
+        var config = style.configBase
+        
+        if let str = title {
+            var container = AttributeContainer()
+            container.font = UIFont.systemFont(ofSize: fontSize, weight: fontWeight)
+            config.attributedSubtitle = AttributedString(str, attributes: container )
+        }
+        config.titleAlignment = .center
+        config.baseBackgroundColor = style.backgroundColor
+        config.baseForegroundColor = style.foregroundColor
+        config.background.strokeColor = style.borderColor
+        config.background.strokeWidth = borderWidth
+        config.background.cornerRadius = borderRadius
+        config.buttonSize = buttonSize
+        
+        return config
     }
 }
 
